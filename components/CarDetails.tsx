7a4ae039e1bfe55a147bb0538930afd8fd0e23ab
Tushar Rajput <tusharo17rajput@gmail.com>
2023-07-08 11:58:05 +0530

    initial commit

---
+++ components/CarDetails.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import { CarDetailsProps } from "@/types"
+
+import { Dialog, Transition } from "@headlessui/react"
+
+const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
+  return <div>CardDetails</div>
+}
+
+export default CarDetails
---
+++ utils/index.ts
@@ -0,0 +1,32 @@
+export async function fetchCars() {
+  const headers = {
+    "X-RapidAPI-Key": "241d931380msh8172069d70a8dd7p1d9528jsneadd146960cc",
+    "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
+  }
+
+  const response = await fetch(
+    "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla",
+    {
+      headers: headers,
+    }
+  )
+
+  const result = await response.json()
+
+  return result
+}
+
+export const calculateCarRent = (city_mpg: number, year: number) => {
+  const basePricePerDay = 50 // Base rental price per day in dollars
+  const mileageFactor = 0.1 // Additional rate per mile driven
+  const ageFactor = 0.05 // Additional rate per year of vehicle age
+
+  // Calculate additional rate based on mileage and age
+  const mileageRate = city_mpg * mileageFactor
+  const ageRate = (new Date().getFullYear() - year) * ageFactor
+
+  // Calculate total rental rate per day
+  const rentalRatePerDay = basePricePerDay + mileageRate + ageRate
+
+  return rentalRatePerDay.toFixed(0)
+}
